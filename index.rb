15f93836ddf1cb37c64fa5078d5561e92f1f5c3f
MSA <crystalbluetech@gmail.com>
2023-08-17 06:41:42 +0100

    put methods

--- index.rb
+++ index.rb
@@ -1,3 +1,29 @@
-puts "hello  world"
+# strings
+puts "hello  world"; puts "this is the begining";
 
-5.times { print "hello  there" }    
+puts "I'm alive and well!";  
+
+
+puts "I bought 5 dollars at the store."
+puts "I made $65 or a profit of 25%!"
+
+# numbers
+
+puts 5
+puts 3.14159
+
+puts 4 + 3 
+
+# concatenation
+puts "4" + "3"
+
+
+puts
+puts
+puts
+
+puts "See, there is a bunch of line brakes above";
+
+3.times { puts } 
+
+puts "3 times puts";
