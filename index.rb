af2e40abab70d29a3c6e2a2438b05761f1619b80
MSA <crystalbluetech@gmail.com>
2023-08-19 15:35:15 +0100

    contants and warning if yo try to re-declare

--- index.rb
+++ index.rb
@@ -1,23 +1,31 @@
-#  parllel var assignment
+# constant
+# Description: This file contains the constant variables for the application
 
-# a = 10
-# b = 20
-# c = 30
-
-# instead you can do it this other  way
-
-a, b, c = 10, 20, 30
-
-
-puts a, b, c
+NAME = "Muhammad"
+LAST_NAME = "Salihu"
+PI = 3.14159
+MY_Age = 25
+School = "ms"
 
 
-# swapping variables
+puts "I am #{NAME} #{LAST_NAME} and i am #{MY_Age} old" 
 
-c, d = 10, 20
 
-puts c, d
+# if you try to change a constant variable in ruby, it will run but with a warning
 
-c, d = d, c
+NAME = "Hauwa"
 
-puts "swapped #{c} for c","And swapped #{d} for d"
+puts NAME
+
+# /Users/muhammadibnsalyhu/Desktop/Ruby/Learn Ruby/index.rb:4: warning: previous definition of NAME was here
+# Hauwa
+
+p NAME # will give a more detailed info
+
+# /Users/muhammadibnsalyhu/Desktop/Ruby/Learn Ruby/index.rb:4: warning: previous definition of NAME was here
+# Hauwa
+# "Hauwa"
+
+
+
+
