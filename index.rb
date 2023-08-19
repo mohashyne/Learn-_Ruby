787caaa8ba2b08f110f50e41be9b2af2a8786d24
MSA <crystalbluetech@gmail.com>
2023-08-19 01:00:45 +0100

    comments

--- index.rb
+++ index.rb
@@ -1,34 +1,14 @@
-# Arithmetic expressions
+# this is a comment
 
-# add
-p 1 + 4
+# put "this as a comment"
 
-# subtract
-p 10 - 6
 
-# multiply
-p 3 * 4
+=begin
+put "i just wrote"
+put "a lot of comments"
+put "in my code"
+=end
 
-# divide
-p 10 / 5
-p 12 / 5
-
-# float , to float you need to make one of the vale a decimal  point
-p 12.0 / 5 
-p 0.5 / 2
-
-# exponent
-p 2 ** 4
-
-# modulo
-p 5 % 2
-
-# class
-p 5.class
-p 0.5.class
-
-# methods
-p 10.9.to_i
-p 10.5.floor
-p 10.5.ceil
-p 3.14159.round(2)
+# put "i just wrote"
+# put "a lot of comments"
+# put "in my code"
