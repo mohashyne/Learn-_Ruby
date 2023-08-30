c5c2655ac14ef9c677d7becd7d6cd05ba81f17de
MSA <crystalbluetech@gmail.com>
2023-08-30 22:06:43 +0100

    learn

--- index.rb
+++ index.rb
@@ -1,31 +1,9 @@
-# constant
-# Description: This file contains the constant variables for the application
+# methods
 
-NAME = "Muhammad"
-LAST_NAME = "Salihu"
-PI = 3.14159
-MY_Age = 25
-School = "ms"
+p "Hello world".length
 
+p "Hello world".upcase
+ 
+# .swapcase is also available, but it's not recommended to use this method because of its name collision with the Ruby class method that
 
-puts "I am #{NAME} #{LAST_NAME} and i am #{MY_Age} old" 
-
-
-# if you try to change a constant variable in ruby, it will run but with a warning
-
-NAME = "Hauwa"
-
-puts NAME
-
-# /Users/muhammadibnsalyhu/Desktop/Ruby/Learn Ruby/index.rb:4: warning: previous definition of NAME was here
-# Hauwa
-
-p NAME # will give a more detailed info
-
-# /Users/muhammadibnsalyhu/Desktop/Ruby/Learn Ruby/index.rb:4: warning: previous definition of NAME was here
-# Hauwa
-# "Hauwa"
-
-
-
-
+p "HI WORLD".swapcase 
