bd45ba4f12c6e516f2075c2ee77e6a6e7a041e3e
MSA <crystalbluetech@gmail.com>
2023-09-01 11:50:37 +0100

    object methods

--- index.rb
+++ index.rb
@@ -6,4 +6,28 @@ p "Hello world".upcase
  
 # .swapcase is also available, but it's not recommended to use this method because of its name collision with the Ruby class method that
 
-p "HI WORLD".swapcase 
+# p "HI WORLD".swapcase 
+
+p "HI WORLD I AM DOWN".downcase 
+
+
+foo = "hello guys"
+
+p foo.upcase
+
+
+# .reverse is also available
+
+p "Hello world".reverse
+
+
+# next : it increaments integer by 1 
+
+p 10.next 
+
+p -3.next  # output -3 + 1 = -2
+
+
+#  inspect
+puts "Hi Everyone";
+puts "Hi Everyone".inspect  # same as  p "Hi Everyone"
