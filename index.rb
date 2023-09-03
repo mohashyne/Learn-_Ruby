16d2d2fd1ad423060c7dac75fa20705a48b04d13
MSA <crystalbluetech@gmail.com>
2023-09-04 00:43:04 +0100

    comparison and equality

--- index.rb
+++ index.rb
@@ -1,23 +1,24 @@
-p 10.odd?  #FalseClass
-p 11.odd?  #TrueClass   
+# comparison & equality
 
-p 2.even?  #TrueClass
-
-
-numbers = [10, 20, 15, 2, 3, 5]
-
-# print odd numbers
-numbers.each do |n|
-    if n.odd?
-        puts "#{n} is an odd number"
-    else n.even?
-        puts "#{n} is an even number"
-    end
-end
+num = 5
+num_string = "5"
 
 
-# p 2.99.odd? #undefine/error because its a float
+p num == num_string.to_i  # true
+p num == num_string.to_f  # true
+p 5 == num_string         # false
+p num == num_string       # false
+p num >= num_string.to_i  # true
 
-p 2.99.to_i
 
 
+
+# JUST PRACTICE
+# nums = (1..10).to_a
+
+# p nums.map {|n| 
+#   if n.odd?
+#     n * 10
+#   else
+#     "even"
+#   end}
