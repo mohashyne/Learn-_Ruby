c4db951949564e86f9b1454c35af663a2f2e36c6
MSA <crystalbluetech@gmail.com>
2023-09-04 18:14:11 +0100

    using step vs upto

---
+++ challenge.rb
@@ -0,0 +1,6 @@
+
+# Challenge
+# Write a loop that iterates over a range of numbers from 1 to 10, and then prints each number multiplied by 2.
+# Expected output:
+
+1.step(10, 1) {|num| puts "#{num} multiplied by 2: #{num * 2}"}
--- index.rb
+++ index.rb
@@ -1,16 +1,31 @@
-# upto and downto accept an argument and also take a block
-# downto
-5.downto(1) {|count| puts "Countdown... #{count}"}
+# step 
+1.step(100, 5) {|number| puts number} # if you  use 0 , it will start from 0
+# 1
+# 6
+# 11
+# 16
+# 21
+# 26
+# 31
+# 36
+# 41
+# 46
+# 51
+# 56
+# 61
+# 66
+# 71
+# 76
+# 81
+# 86
+# 91
+# 96
 
-5.downto(0) do |i| 
-    puts "We are currently on #{i}"
-    puts "Hooray"
+0.step(85, 7) do |num|
+    puts "Alright, let's go up by 7 again!"
+    puts "I'm now on #{num}"
 end
 
-puts "LIFTOFF"
-
-
-
-
-# upto
-1.upto(10) {|count| puts "*Counting... #{count}"}
+# step and upto method
+0.step(10, 1) {|num| puts "Alright, let's go up by step again! I'm now on #{num}"}
+0.upto(10) {|n| puts "Alright, let's go up with upto! I'm now on #{n}"}
