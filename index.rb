ae63c8c982b41fce2d366bd4d9948e29942848a6
MSA <crystalbluetech@gmail.com>
2023-09-04 11:21:53 +0100

    using arithmethic methods e.g p1.+1, p10.modulo(3)

--- index.rb
+++ index.rb
@@ -1,36 +1,20 @@
-# comparison & equality
+# Arithmetic method
 
-num = 5
-num_string = "5"
-num_float = 5.0
-
-
-p num == num_string.to_i  # true
-p num == num_string.to_f  # true
-p 5 == num_string         # false
-p num == num_string       # false
-p 5 == 5.0 
-
-# greater than equal to
-p num >= num_string.to_i  # true
-
-# less than equal to
-p num <= num_string.to_i  # true
-    
-
-# not equal to
-p 5 != 5.0     # false
+# diff input with same output
+p 1.next()
+p 1 + 1
+p 1.+(1)
+p 1.+1
 
 
+# example 2
+p 2.next()
+p 1 + 2
+p 1.+(2)
+p 1.+2
 
+# example 3
+p 10 % 3
+p 10.%(3)
+p 1.modulo(3)
 
-
-# JUST PRACTICE
-# nums = (1..10).to_a
-
-# p nums.map {|n| 
-#   if n.odd?
-#     n * 10
-#   else
-#     "even"
-#   end}
