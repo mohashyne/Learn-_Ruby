739f5cb38d3753adaedac9d3b20b2eb816b808fd
MSA <crystalbluetech@gmail.com>
2023-09-04 14:57:00 +0100

    upto and downto method

--- index.rb
+++ index.rb
@@ -1,33 +1,16 @@
-5.times {puts "I Love  Ruby"} # curly braces for a 1 line syntax
-5.times { |count| puts "We are on number #{count}"}
+# upto and downto accept an argument and also take a block
+# downto
+5.downto(1) {|count| puts "Countdown... #{count}"}
 
-3.times do
-    puts "My name is Muhammad"
-    puts "I am a Nigerian"
-end    #  do  end for multiple lines (recommended) but interchangable
+5.downto(0) do |i| 
+    puts "We are currently on #{i}"
+    puts "Hooray"
+end
 
-
-# block  variable ||
-3.times do   |count|
-    # puts "loop number: #{count}"
-
-#to  start count from 1 instead of 0
-    puts "loop number: #{count.+1}"
-    puts "Student"
-    puts "School"
-end    #  do  end for multiple lines (recommended) but interchangable
-
-# puts count # error because its a block scope
+puts "LIFTOFF"
 
 
 
 
-# 2.times {puts "Enter your password:"
-# password = gets.chomp
-# if password == "muhammad"
-#   puts "Greetings, Professor Muhammad. Would you like to play a game?"
-#   break
-# else
-#   puts "Access denied."
-# end
-# }
+# upto
+1.upto(10) {|count| puts "*Counting... #{count}"}
