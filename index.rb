abceb8d6db7c39eb5fa779a361dee032f48d26d4
MSA <crystalbluetech@gmail.com>
2023-09-04 20:15:49 +0100

    creating strings

--- index.rb
+++ index.rb
@@ -1,31 +1,19 @@
-# step 
-1.step(100, 5) {|number| puts number} # if you  use 0 , it will start from 0
-# 1
-# 6
-# 11
-# 16
-# 21
-# 26
-# 31
-# 36
-# 41
-# 46
-# 51
-# 56
-# 61
-# 66
-# 71
-# 76
-# 81
-# 86
-# 91
-# 96
+# space = " "
+# p space.length # 1
 
-0.step(85, 7) do |num|
-    puts "Alright, let's go up by 7 again!"
-    puts "I'm now on #{num}"
-end
+# empty = ""
+# p empty.length # 0
 
-# step and upto method
-0.step(10, 1) {|num| puts "Alright, let's go up by step again! I'm now on #{num}"}
-0.upto(10) {|n| puts "Alright, let's go up with upto! I'm now on #{n}"}
+# p empty.class # string
+# p empty.empty? # true
+# p space.empty? # false
+
+# ultimate syntax to create a string
+p String.new # ""
+p String.new.class # String
+
+name = String.new("Muhammd")
+
+p name # "Muhammd"
+
+p 5.to_s # "5"
