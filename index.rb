fc2dddc72d8ab8e2fe1fc5aa773f14d9189a326b
MSA <crystalbluetech@gmail.com>
2023-09-04 12:09:51 +0100

    re-assignment, increment  and decrement

--- index.rb
+++ index.rb
@@ -1,23 +1,17 @@
-# float methods
+a = 10
+p a
 
-# floor
-p 3.5.floor # 3
+# a =  a  + 5
+a += 5
+p a
 
-# ceil
-p 3.5.ceil # 4
-p 3.14.ceil # 4
+p "..................................................."
 
-# round
-p 3.14159.round # 3 , this  follows the mathematical rule 
-# if after the decimal point  is less  than .5  it dosn't round up
-# it also rounds the  number of digits you want to see after the decimal point
-p 3.6 # 4
-p 3.14159.round(2) # 3.14
-p 3.14159.round(3) # 3.142 , 1 is round to 2 because 5 is next to it
+b = 100
+p b
 
-# abs
-p 35.67.abs # 35.67
-p -35.67.abs # 35.67
-p 35.67.abs.class # Float
-p -50.abs # 50
+# b =  a  + 50
+b -= 50
+p b
+
 
