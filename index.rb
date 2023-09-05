1cca523e6d196a4d8151b3e99312d6cb7c924e43
MSA <crystalbluetech@gmail.com>
2023-09-06 00:40:54 +0100

    return values using implicit and explicit

--- index.rb
+++ index.rb
@@ -1,23 +1,36 @@
-# parameter : 
-# parameters  can  be more than one
+# def add_two_num(num1, num2)
+#   return num1 + num2  # Explicit
+# end
 
-def praise_person(name) # name is the parameter
-    puts "#{name} is amazing"
-    puts "#{name} is talented"
-    puts "#{name} is brilliant"
+
+# result = add_two_num(3, 8)
+# p result
+
+
+# THE RETURN KEYWORD TERMINATES AN OPERATION
+# IF THERE IS ANY OPERATION BELOW IT WILL NOT RUN
+
+def add_two_num(num1, num2)
+  puts "This is implicit"
+  num1 + num2  # implicit
 end
 
-praise_person("Muhammad") # "muhammad"  is the argument
-praise_person("Hassan") # "Hassan"  is the argument
-praise_person("Umar")  # "Umar"  is the argument
 
-# output
-# Muhammad is amazing
-# Muhammad is talented
-# Muhammad is brilliant
-# Hassan is amazing
-# Hassan is talented
-# Hassan is brilliant
-# Umar is amazing
-# Umar is talented
-# Umar is brilliant
+result = add_two_num(3, 8)
+p result
+
+
+def sum_numbers(n1, n2)
+
+        puts "Enter first number"
+        n1 = gets.chomp.to_i
+        puts "Enter second number"
+        n2 = gets.chomp.to_i
+        return puts "The Total is #{n1 + n2}"
+        
+end
+
+sum_numbers(3, 3)
+
+
+# Explicit meant including the return keyword and implicit is the opposite
--- tempCodeRunnerFile.rb
+++ tempCodeRunnerFile.rb
@@ -1,16 +1,7 @@
-p story.length
-# p story[1..51]
-# p story.slice(-1)
+def add_two_num(num1, num2)
+#   return num1 + num2
+# end
 
-# # story[27..39] = "Ruby programmers "
-# # p story
 
-# p story[6] # "p"
-# p story[6, 10] # "pon a time"
-# p story.slice(8) # "n"
-# p story.slice(8, 10) # "n a time i"
-# p story.slice(8..10) # "n a"
-# p story[-1] # h
-# p story[0, story.length] # pull out the  eniter string
-# p story[0, story.length].slice(0) # O
-
+# result = add_two_num(3, 8)
+# p result
