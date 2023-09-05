b865ad0a03081bea9a8ea8746c723a1947c4b78e
MSA <crystalbluetech@gmail.com>
2023-09-05 13:53:54 +0100

    using range .. and ...

--- index.rb
+++ index.rb
@@ -1,4 +1,11 @@
 story = "Once upon a time in a land, where programmers laugh"
 
-p " Extract 6 char from right: " , story[-13, 5] # "ramme"
+# two .. (char 28 is inclusive)
+p story[17..28] # "in a land, w"
+p story.slice(17..28)  # "in a land, w"
+
+# two .. (char 28 is Exclusive)
+p story[17...28] # "in a land, "
+p story.slice(17...28)  # "in a land, "
+p story[17..100] # "in a land, where programmers laugh"
 
