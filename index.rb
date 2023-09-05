c17f4fca9f9a32fdea1d5e63a0b45f2d1363b9ab
MSA <crystalbluetech@gmail.com>
2023-09-05 13:14:17 +0100

    using square bracket and slice braces to extract or alter strings

--- index.rb
+++ index.rb
@@ -1,4 +1,8 @@
 story = "Once upon a time in a land, where programmers laugh"
+p story.length
+p story[1..51]
+p story.slice(-1)
+
 # story[27..39] = "Ruby programmers "
 # p story
 
@@ -7,3 +11,7 @@ p story[6, 10] # "pon a time"
 p story.slice(8) # "n"
 p story.slice(8, 10) # "n a time i"
 p story.slice(8..10) # "n a"
+p story[-1] # h
+
+# computer programmes start counting from 0, 
+# and if you u are using negetive number it start from 1
