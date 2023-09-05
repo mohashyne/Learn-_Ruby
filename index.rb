a991f08fd06266cace9627a00ba705a003926729
MSA <crystalbluetech@gmail.com>
2023-09-05 13:43:27 +0100

    using square bracket and slice braces to extract or alter strings

--- index.rb
+++ index.rb
@@ -1,17 +1,4 @@
 story = "Once upon a time in a land, where programmers laugh"
-p story.length
-p story[1..51]
-p story.slice(-1)
 
-# story[27..39] = "Ruby programmers "
-# p story
+p " Extract 6 char from right: " , story[-13, 5] # "ramme"
 
-p story[6] # "p"
-p story[6, 10] # "pon a time"
-p story.slice(8) # "n"
-p story.slice(8, 10) # "n a time i"
-p story.slice(8..10) # "n a"
-p story[-1] # h
-
-# computer programmes start counting from 0, 
-# and if you u are using negetive number it start from 1
--- tempCodeRunnerFile.rb
+++ tempCodeRunnerFile.rb
@@ -1,1 +1,16 @@
-a
+p story.length
+# p story[1..51]
+# p story.slice(-1)
+
+# # story[27..39] = "Ruby programmers "
+# # p story
+
+# p story[6] # "p"
+# p story[6, 10] # "pon a time"
+# p story.slice(8) # "n"
+# p story.slice(8, 10) # "n a time i"
+# p story.slice(8..10) # "n a"
+# p story[-1] # h
+# p story[0, story.length] # pull out the  eniter string
+# p story[0, story.length].slice(0) # O
+
