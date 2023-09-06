33c0361f6874fdb9d03d3310082276222332358a
MSA <crystalbluetech@gmail.com>
2023-09-06 23:33:59 +0100

    using prntheses and precedence

--- User_input.rb
+++ User_input.rb
@@ -1,7 +1,12 @@
-budget = 5
-price = 10
-mood = "happy"
 
-if budget > price || mood == "happy"
-    puts "I'm going to buy the item!"
+def authenticate_agent(rank, name, credentials)
+    if (rank == "007" && name == "James Bond") || credentials == "Secret Agent"
+        puts "Access granted, please proceed to Intelligence department!"
+    else
+        puts "Access denied, #{name}"
+    end
 end
+
+authenticate_agent("007", "James Bond", "Spy")
+authenticate_agent("007", "Jim Bond", "Spy")
+authenticate_agent("006", "Jim Bond", "Spy")
