b80177250492a69de94eac9d091af19ae6a88250
MSA <crystalbluetech@gmail.com>
2023-09-06 22:37:16 +0100

    multiple conditions

--- User_input.rb
+++ User_input.rb
@@ -1,36 +1,9 @@
+age = 30
+ticket = nil
+id = true
 
-color = "blue"
-
-# if color.include?("Red")
-#   puts "Red is included in the string"
-# else
-#   puts "Red is not included in the string"
-# end
-
-if color.include?("Red")
-    puts "Red is included in the string"
-elsif
-     color == "Red" 
-     puts "Red is not included in the string"
-else
-    puts "its Neither red nor green"
-end
-
-array = [2,7,6,4,2,4,9,7]
-
-array.each do |num| 
-    if num.odd?
-        puts "#{num} is odd"
+if age > 21 && ticket && id
+    puts "Congratulations, welcome to the show!"
     else
-        puts "#{num} is even"
-    end
-end
- 
-
-def odd_or_even(num)
-    if num.odd?
-        puts "#{num} is odd"
-    else
-        puts "#{num} is even"
-    end
+    puts "Sorry, you can't come in."
 end
