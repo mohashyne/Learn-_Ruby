dc1aadb6ace9d7904f5963962a22b1abd7de485b
MSA <crystalbluetech@gmail.com>
2023-09-07 22:10:16 +0100

    case-when-else finally

--- index.rb
+++ index.rb
@@ -16,3 +16,18 @@ def rate_my_food(food)
 end
 
 puts rate_my_food("Sushi")
+
+
+
+def calculate_school_grade(grade) 
+    case grade
+    when 90..100 then "A"
+    when 80..89 then "B"
+    when 70..79 then "C"
+    when 60..69 then "D"
+    else "F"
+    end
+end
+
+puts "Your grade is  #{calculate_school_grade(95)}"
+# Your grade is  A
