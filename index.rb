4cf9adab5d09f0fd1174cceebbaab542aa7ed440
MSA <crystalbluetech@gmail.com>
2023-09-08 00:00:07 +0100

    using single negation to output the opposit and double to maintain the state

--- index.rb
+++ index.rb
@@ -1,33 +1,17 @@
-# Case
+# Negation
 
-def rate_my_food(food)
-    case food
-    when "Steak"
-        "Pass the steak sauce! That's delicious!"
-        
-    when "Sushi"
-        "Great choice! My favorite food" 
-    when "Tacos"
-        "Cheesy and filling! The perfect combination"    
-    else
-        "I don't know about that food!"
-    end
-    
+# Negation is a unary operator that returns the opposite of its operand.
+# For example, !true returns false, and !false returns true.
+puts !true # => false
+puts !false # => true
+
+# The following expression evaluates to 1 because it's equivalent to (0 + 2) * -3:
+puts -(0 + 2) * -3 # => 6
+
+user = "free"
+if user != "subscriber"
+    puts "Only subscribers are allowed!"
 end
 
-puts rate_my_food("Sushi")
-
-
-
-def calculate_school_grade(grade) 
-    case grade
-    when 90..100 then "A"
-    when 80..89 then "B"
-    when 70..79 then "C"
-    when 60..69 then "D"
-    else "F"
-    end
-end
-
-puts "Your grade is  #{calculate_school_grade(95)}"
-# Your grade is  A
+p !!1
+p !!false # false , because  we used  double  negation
