82474370a6bb2a44d79680aa5362758fc7cc8acf
MSA <crystalbluetech@gmail.com>
2023-09-07 12:37:11 +0100

    using ternery operator to make codes shorter

--- User_input.rb
+++ User_input.rb
@@ -1,43 +1,19 @@
-# we  are going to check if the variable responds to a method or something else
-price = 50
+# ternary operator
 
-p price.respond_to?("hello") # false
-p price.respond_to?("next") # true
-p price.respond_to?("odd?") # true
-p price.respond_to?("even?") # true
-p price.respond_to?("even?") # true
-p price.respond_to?("length?") # false
-p price.respond_to?("each") # false
+# This is a longer code
+puts "Enter a number: "
+num = gets.chomp.to_i
+if num > 0
+    puts "Your number is positive"
+    else
+    puts "Your number is negative"
+    end
 
-
-budget = 100
-
-if budget.respond_to?("length") 
-    p budget.length
-else 
-    p "budget cannot respond to length"
-end
+# This is a shorter code     # ternary operator (simplified)
+puts "Enter a number: "
+num = gets.chomp.to_i
+num > 0 ? puts("Your number is positive") : puts("Your number is negative")
 
 
-puts "hello".respond_to?("length") # trus
-puts "hello".respond_to?("class") # trus
-puts "hello".respond_to?("upcase") # trus
 
-# using symbol instead of string
-puts "hello".respond_to?(:length) # true
-puts "hello".respond_to?(:class) # true
-puts "hello".respond_to?(:upcase) # true
-
-# Path: User_input.rb
-# we  are going to check if the variable responds to a method or something else
-num = 1000
-
-if num.respond_to?(:next)
-    p num.next
-end
-
-puts "hello".respond_to?(:next) # false
-puts "hello".respond_to?(:odd?) # false
-puts "hello".respond_to?(:even?) # false
-puts "hello".respond_to?(:length?) # false
-puts "hello".respond_to?(:each) # false
+    
