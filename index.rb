e644ad2f973d69bf7a863fae15cb5456ae48aa64
MSA <crystalbluetech@gmail.com>
2023-09-11 09:43:45 +0100

    inline statement modifiers

--- index.rb
+++ index.rb
@@ -1,18 +1,36 @@
-# if = unsles and while = until
-# until: means continue to execute UNTIL condition is true
+# inline modifiers
 
+number = 5000
 
-# i = 1
-
-# while i < 10
-#     puts i
-#     i += 1
+# if number > 2500
+#     puts "Huge number!"
 # end
 
+puts "Huge number!" if number > 2500
 
-i = 1
+puts "Huge number!" unless number < 2500
 
-until i > 9
-    puts i
-    i += 1
+# ternary operator
+puts number > 2500 ? "Huge number!" : "Small number!"
+
+
+
+# case statement
+
+# case number
+# when 2500
+#     puts "it is 2500"
+# when 5000
+#     puts "it is 5000"
+# when 7500
+#     puts "it is 7500"
+# else
+#     puts "it is not 2500, 5000, or 7500"
+# end
+
+case number
+when 2500 then puts "it is 2500"
+when 5000 then puts "it is 5000"
+when 7500 then puts "it is 7500"
+else puts "it is not 2500, 5000, or 7500"
 end
