05b044295fd0372046b056f4310ad11a23e4130c
MSA <crystalbluetech@gmail.com>
2023-09-11 11:16:33 +0100

    size method

--- index.rb
+++ index.rb
@@ -1,32 +1,14 @@
-#  Alphabetical range
-alphabet = "a".."z"
-p alphabet
+# size method only works on numbers
+# string  can be converts to numbers to work i.e "5"
 
-puts alphabet.first(5)
-puts alphabet.last(3)
+# numbers = 143...700  # if you use 3 dots ... the last number will be excluded 
 
+numbers = 143..700
 
-# mixing Capital letters and small letters, Caps comes first
-# remembeer if you do  something like this below
-# it  will give you  extra strings befor the small letters starts
-
-alphabetCaps_small = "A".."z"
-p alphabetCaps_small.first(40)
-
-#["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L",
-# "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X",
-# "Y", "Z",
-# "[", "\\", "]", "^", "_", "`", 
-# "a", "b", "c", "d", "e", "f", "g", "h"]
-
-# CAPS ONLY
-alphabetCaps = "A".."Z"
-p alphabetCaps.first(40)
-
-#["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+p numbers.size # 558
+p numbers.count # 558
+p numbers.first(557)
+p numbers.last(1)  # 700
 
 
 
-
-
-# 1. Create a method that takes a range of letters and returns each letter in the range except for the last letter
