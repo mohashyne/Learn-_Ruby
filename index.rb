4e09f13a1f1a3cccf021c05ef858d7a8cfd07e75
MSA <crystalbluetech@gmail.com>
2023-09-14 23:41:50 +0100

    overrite array elements

--- index.rb
+++ index.rb
@@ -1,13 +1,40 @@
-nummbers = [0, 2, 4, 8, 10, 12, 14]
+fruits = ["Apple", "Orange", "Grape", "Banana"]
 
-p nummbers.slice(3) # 8
-p nummbers.slice(2, 3) # [4, 8, 10]
-p nummbers.slice(4..6) # [10, 12, 14]
-p nummbers.slice(4...6) # [10, 12]
-p nummbers.slice(-1) # 14
-p nummbers.slice(100) # nil
-p nummbers.slice(2, 100) # [4, 8, 10, 12, 14]
-p nummbers.slice(100, 2) # nil
-p nummbers.slice(2..100) # [4, 8, 10, 12, 14]
-p nummbers.slice(100..200) # nil
-p nummbers.slice(2...100) # [4, 8, 10, 12, 14]
+fruits[3] = "Yoghurt"
+
+p fruits   # ["Apple", "Orange", "Grape", "Yoghurt"]
+
+# ADD
+fruits[4] = "juice"
+
+p fruits   # ["Apple", "Orange", "Grape", "Yoghurt", "juice"]
+
+fruits[10] = "Kiwi"
+p fruits  #  ["Apple", "Orange", "Grape", "Yoghurt", "juice", nil, nil, nil, nil, nil, "Kiwi"]
+
+# ADDING MULTIPLE ELEMENTS
+fruits[5, 3] = ["Canteloupe", "Dragonfruit", "Starfruit"]
+p fruits  # ["Apple", "Orange", "Grape", "Yoghurt", "juice", "Canteloupe", "Dragonfruit", "Starfruit", nil, "Kiwi"]
+
+# REMOVE
+fruits[9] = []
+p fruits
+
+
+# REPLACE USING CONDITIONAL ASSIGNMENT
+fruits2 = ["Apple", "Orange", "Grape", "Banana"]
+p fruits2
+
+# fruits[5] = "Watermelon" if fruits[5] == nil
+      
+     # OR
+
+fruits2[4] ||= "Watermelon" 
+p fruits2   
+
+fruits2[0..2] = ["blackberry", "kiwi"]
+p fruits2 # ["blackberry", "kiwi", "Banana", "Watermelon"]
+
+fruits2[0..2] = ["blackberry", "kiwi", "orange", "banana", "grape"]
+p fruits2  # ["blackberry", "kiwi", "orange", "banana", "grape", "Watermelon"]
+
--- tempCodeRunnerFile.rb
+++ tempCodeRunnerFile.rb
@@ -1,4 +1,1 @@
-["he has 2 apples"]
-# ["he has 4 apples"]
-# ["he has 6 apples"]
-# ["he has 8 apples"]
+mice
