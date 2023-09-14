f72c16cc632a0095f91bc7d3ef727a21bdc5c7df
MSA <crystalbluetech@gmail.com>
2023-09-14 20:46:06 +0100

    using bracket-range to values for sequential array  extraction

--- index.rb
+++ index.rb
@@ -1,29 +1,7 @@
+numbers = %w[1 2 3 4 6 8 9 10 4]
 
-# ASSESSING SINGLE ARRAY ELEMENT BY  USING FETCH METHOD
-fruits = ["Apple", "Orange", "Grape", "Books", 2, 8, 9]
+p numbers[1..4] # ["2", "3", "4", "6"]
+p numbers[1...4] # ["2", "3", "4"]
 
-
-# a new subset array will be created
-p fruits[2, 3] #  ["Grape", "Books", 2]
-
-
-p fruits[0] # Apple 
-p fruits[0].class # string
-
-p fruits[0, 1] # ["Apple"]
-p fruits[0, 1].class # Array
-
-
-#   exceeding the number  of elements  and results
-#   in nil
-p fruits[10] # nil
-p fruits[10].class # NilClass
-    
-
-    p fruits[4, 100] # [2, 8, 9]
-    # it will get the  available and cut off
-  
-
-
-
-
+# this will get everything if the array dosn't reach 100
+p numbers[1..100] #["2", "3", "4", "6", "8", "9", "10", "4"]
