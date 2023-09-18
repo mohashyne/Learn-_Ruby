d31f3d3bff70eea896a87dec0afb1f981503af6a
MSA <crystalbluetech@gmail.com>
2023-09-18 12:15:19 +0100

    using empty and nil

--- index.rb
+++ index.rb
@@ -1,16 +1,63 @@
-# length
-p [1, 2, 3, 4, nil, "hello"].length #  6
-p [].length  # 0
+puts [1, 2, 3].empty? # false
 
-# size
-p [1, 2, 3, 4, nil, "hello"].size # 6
-p [].size  # 0
+puts [].empty?  # true
+
+puts [].empty? == 0 #  true
 
 
-#  count
-p [1, 2, 3, 4, nil, "hello", 2, 6, 2, 4, 1, nil, nil, nil].count(nil) #  4
-p [1, 2, 3, 4, nil, "hello", 2, 6, 2, 4, 1, nil, nil, nil].count # 14
+fullNum = [1, 2, 3, 4, 5, 6, 7, 2, 8, 9,  2, 0]
+if !fullNum.empty? 
+     puts fullNum.count(2)
+end
 
-#  count is a little bit more flexible
+letters  = ("a".."j").to_a
+p letters  # ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
+p letters[50].nil? # true
 
 
+# nil
+# nil is a special value that means "nothing". It is not the same as false, and it is not 0. For example, if you have a variable that has not been assigned a value yet, it will be nil:
+ puts [].nil? # false
+ num = [7, 8, 0, 9]
+     puts num.nil? # false
+     puts num[100].nil? # true
+
+     # x = nil
+     # puts x.nil? # true
+     # puts x == nil # true
+     # puts x == false # false
+     # puts x == 0 # false
+     # puts x == "" # false
+     # puts x == [] # false
+     # puts x == {} # false
+     # puts x == () # false
+     # puts x == 1 # false
+     # puts x == "Hello" # false
+     # puts x == [1, 2, 3] # false
+     # puts x == {a: 1, b: 2, c: 3} # false
+     # puts x == Object # false
+     # puts x == Class # false
+     # puts x == Module # false
+     # puts x == BasicObject # false
+     # puts x == true # false
+     # puts x == false # false
+     # puts x == 0.0 # false
+     # puts x == 0.1 # false
+     # puts x == 1.0 # false
+     # puts x == 1.1 # false
+     # puts x == -1.0 # false
+     # puts x == -1.1 # false
+     # puts x == 1.0 / 0.0 # false
+     # puts x == -1.0 / 0.0 # false
+     # puts x == 0.0 / 0.0 # false
+     # puts x == 0.0 / 0.0 # false
+     # puts x == 0.0 / 0.0 # false
+     # puts x == 0.0 / 0.0 # false
+     # puts x == 0.0 / 0.0 # false
+     # puts x == 0.0 / 0.0 # false
+     # puts x == 0.0 / 0.0 # false
+     # puts x == 0.0 / 0.0 # false
+     # puts x == 0.0 / 0.0 # false
+     
+     # puts x == 0.0 / 0.0 # false
+     # puts x == 0.0 / 0.0 # false
