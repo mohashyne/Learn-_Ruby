1903b3e67f2a23ac9a3579a56a631c067bccdf8b
MSA <crystalbluetech@gmail.com>
2023-09-19 12:10:47 +0100

    pop method and how to remove mutiple items

--- index.rb
+++ index.rb
@@ -1,14 +1,19 @@
 location = ["House", "Airpot", "Market"]
 p location # ["House", "Airpot", "Market"]
 
-location.push("Hospital")
-p location # ["House", "Airpot", "Market", "Hospital"]
+# print this will enable us see the  item that was removed
+last_item_as_string = location.pop()
+p location   # ["House", "Airpot"] # ["House", "Airpot"]
+p last_item_as_string #  ["House"]
 
-location << "School"
-p location # ["House", "Airpot", "Market", "Hospital", "School"]
+last_item_as_array = location.pop(1)
+p last_item_as_array  # item removed "Market"
 
-location.insert(1, "Park")
-p location # ["House", "Park", "Airpot", "Market", "Hospital", "School"]
 
-location.insert(3, "Car Wash")
-p location # ["House", "Park", "Airpot", "Car Wash", "Market", "Hospital", "School"]
+# location.push("School")
+# p location  # ["House", "Airpot", "School"]
+
+# two_items = location.pop(2)
+# p location  #  ["House"]
+# p two_items  # ["Airpot", "School"]
+
---
+++ tempCodeRunnerFile.rb
@@ -0,0 +1,7 @@
+
+# location.push("School")
+# p location  # ["House", "Airpot", "School"]
+
+# two_items = location.pop(2)
+# p location  #  ["House"]
+# p two_items  # ["Airpot", "School"]
