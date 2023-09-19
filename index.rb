39945c01751b8baa0340235b460b0a6aaa8d07e6
MSA <crystalbluetech@gmail.com>
2023-09-19 11:46:22 +0100

    Adding elements to an array using push, shovel sign and insert

--- index.rb
+++ index.rb
@@ -1,30 +1,14 @@
-arr = [1, 3, 5, 7,9, 15, 21, 18, 6]
+location = ["House", "Airpot", "Market"]
+p location # ["House", "Airpot", "Market"]
 
-p arr.first # 1
-p arr.last # 6
+location.push("Hospital")
+p location # ["House", "Airpot", "Market", "Hospital"]
 
-p arr.first(3) # [1, 3, 5]
+location << "School"
+p location # ["House", "Airpot", "Market", "Hospital", "School"]
 
-p arr.first(1) # 1
-p arr.last(1) # 6
+location.insert(1, "Park")
+p location # ["House", "Park", "Airpot", "Market", "Hospital", "School"]
 
-p arr.first(4)[2] # 5
-p arr.last(4)[0] # 15
-
-def first(arr, num = 0)
-return arr[0] if num == 0
-arr[0, num]
-end
-
-p first(arr) # 1
-p first(arr, 5) # [1, 3, 5, 7, 9]
-p first(arr, 2) # [1, 3]
-
-
-def custom_last(arr, num = 0)
-  return arr[-1] if num == 0
-     arr[-num..-1]
-end
-
-p custom_last(arr) # 6
-p custom_last(arr, 5) # [21, 18, 6]
+location.insert(3, "Car Wash")
+p location # ["House", "Park", "Airpot", "Car Wash", "Market", "Hospital", "School"]
