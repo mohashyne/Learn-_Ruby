71bc32cd94f4d7ed1ad65b29e1c76de228273822
MSA <crystalbluetech@gmail.com>
2023-09-20 11:34:08 +0100

    convert range to array

--- index.rb
+++ index.rb
@@ -1,47 +1,12 @@
-# SpaceShip operator  <=>
+# letters range 
+letters_range = "A".."T"
+p letters_range #  "A".."T"
+letters_range  = letters_range.to_a
+p letters_range # ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T"]
 
-# The spaceship operator is a syntactic sugar for the comparison operator.
-
-# It returns 0 if the first operand equals the second one, 1 if the first operand is greater than the second one, and -1 if the first operand is less than the second one.
-
-# Example:
-
-# 1 <=> 1 # 0
-# 1 <=> 2 # -1
-# 2 <=> 1 # 1
-# 2 <=> 2 # 0
-# 1 <=> '1' # nil
-p [3, 4, 5] <=> ["hello", 3, 5] # nil
-p [3, 4, 5] <=> [3, 4, 4] # 1
-p [3, 4, 5] <=> [3, 4, 9] # -1
-
-
-# The spaceship operator is useful when you want to compare two values but don't know their type. It's also useful when you want to sort an array.
-
-# Example:
-
-# [1, 2, 3].sort { |a, b| b <=> a } # [3, 2, 1]
-
-# Task
-
-# Build a method that takes two arguments of the same type and returns an array of integers 1 or -1 depending on whether the first argument is less than or greater than the second argument.
-
-# Examples:
-
-# spaceship(2, 4) # [-1, -1]
-# spaceship(4, 4) # [0, 0]
-# spaceship(4, 2) # [1, 1]
-# spaceship('a', 'z') # [-1, -1]
-# spaceship('z', 'a') # [1, 1]
-# spaceship('a', 'a') # [0, 0]
-# spaceship(2, 'z') # nil
-# spaceship('a', 2) # nil
-
-def spaceship(a, b)
-  return nil unless a.class == b.class
-  return [0, 0] if a == b
-  a > b ? [1, 1] : [-1, -1]
-end
-
-p spaceship(2, 4) # [-1, -1]
-p spaceship(4, 4) # [0, 0
+# numbers range
+numrange_to_array = 1..10
+p numrange_to_array # 1..10
+numrange_to_array = numrange_to_array.to_a
+p numrange_to_array # [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+p numrange_to_array.length # 10
