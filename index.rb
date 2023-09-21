9b2874dfca61b51cfb53a82e70cf7b36dfee339f
MSA <crystalbluetech@gmail.com>
2023-09-21 14:57:41 +0100

    using ri(ruby index) to see documentattion

--- index.rb
+++ index.rb
@@ -1,54 +1,45 @@
-# unlike cllass
-# def check_class(element)
-#      puts "It's a class! of #{element.class}"
-# return element.class 
-# end
+# ri stands for "Ruby Index". It is a command-line tool that provides access to Ruby's documentation from the command line. It allows you to look up information about classes, modules, methods, and more directly in your terminal.
 
-# check_class("5") # It's a class! of String
+# Here are some common ri commands:
+
+#     ri <class or module>: Look up information about a specific class or module.
+#     ri <class or module>.<method>: Look up information about a specific method within a class or module.
+#     ri -c: List all available classes and modules.
+#     ri -m: List all available methods.
+
+# For example, if you want to learn about the Array class, you can type:
+
+# javascript
+
+# ri Array
+
+# This will display detailed information about the Array class, including its methods and usage.
+
+# Keep in mind that ri relies on locally installed Ruby documentation. If you find that ri is not working or you don't have the documentation installed, you may need to set up the documentation using tools like rdoc or yard.
 
 
-# isa predicate
+# ri Array.values_at                                                        ─╯
 
-puts 2.is_a?(String) # false
-puts 2.is_a?(Array) # false
-puts 2.is_a?(Numeric) # true
-puts 2.is_a?(Integer) # true
-puts [4, 5, 6].is_a?(Array) # true
+# = Array.values_at
 
-arr = [7,9,0]
-if arr.is_a?(Array)
-     arr.each {|e| puts e}
-else
-     puts "This is not an array" 
-end
+# (from ruby core)
+# ------------------------------------------------------------------------
+#   array.values_at(*indexes) -> new_array
 
-# elements are object  and BasicObject in the higher level
-p "a".is_a?(Object)
-p "a".is_a?(BasicObject)
+# ------------------------------------------------------------------------
 
-# In Ruby, both Object and BasicObject are fundamental classes in the object-oriented hierarchy, but they serve different purposes and have different levels of functionality and inheritance.
+# Returns a new Array whose elements are the elements of self at the given
+# Integer or Range indexes.
 
-#     Object Class:
-#         Object is the root class for most Ruby objects. Almost every class in Ruby implicitly inherits from Object.
-#         It provides a wide range of methods and functionality to all Ruby objects, such as to_s (for string representation), inspect (for debugging), and many others.
-#         When you define a custom class in Ruby without specifying a superclass, it will automatically inherit from Object.
-#         You can override or extend methods inherited from Object in your custom classes.
+# For each positive index, returns the element at offset index:
 
-#     BasicObject Class:
-#         BasicObject is even more fundamental than Object. It is a much simpler class with very limited built-in methods.
-#         It provides a minimal set of methods, including ==, !=, !, and __send__, but it lacks many of the common methods found in Object.
-#         BasicObject is useful when you want to create objects with the absolute minimum set of methods, typically for proxy objects or to implement custom method handling.
+  a = [:foo, 'bar', 2]
+  a.values_at(0, 2) # => [:foo, 2]
+  a.values_at(0..1) # => [:foo, "bar"]
 
-# Here's a basic hierarchy:
+# The given indexes may be in any order, and may repeat:
 
-             # BasicObject
-             #    ^
-             #    |
-             #  Object
-             #    ^
-             #    |
-             #  Your Custom Classes
+  a = [:foo, 'bar', 2]
+  a.values_at(2, 0, 1, 0, 2) # => [2, :foo, "bar", :foo, 2]
+  a.values_at(1, 0..2) # => ["bar", :foo, "bar", 2]
 
-# In most cases, you'll work with classes that inherit from Object because it provides a broad set of methods and functionality. However, if you have specific use cases where you need to strip down an object to its bare essentials, you might use BasicObject as a superclass. It's important to choose the appropriate level of functionality based on your requirements when designing your classes in Ruby.
-
- # NOTE Fixnum and Bignum are deprecated on the latest ruby, use Integer for all 
