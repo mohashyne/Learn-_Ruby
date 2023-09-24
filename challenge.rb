4ca4c2dc11a0494e14552ac874f5828166135a59
MSA <crystalbluetech@gmail.com>
2023-09-25 00:09:19 +0100

    each_with_index

--- challenge.rb
+++ challenge.rb
@@ -1,37 +1,9 @@
-# fizzbuzz challenge
+# write a loop that iterates over a number array
+# Output the PRODUCT of each number and its index position
 
+fives = [5, 10, 15, 20, 25]
 
-def fizzbuzz(number)
-if number % 3 == 0 && number % 5 == 0
-    puts "FizzBuzz #{number} is divisible 3 and 5"
-elsif number % 3 == 0
-puts "Buzz #{number} is divisible 3"
-    elsif number % 5 == 0
-        puts "Fizz #{number} is divisible 5"
-    else
-        puts number
-    end
-end
-
-fizzbuzz(15)
-
-
-# solution using while
-def fizzbuzz_w(number)
-    i = 1
-    # while i <= number
-    until i > number
-        if i % 15 == 0
-            puts "FizzBuzz #{i} is divisible 3 and 5"
-        elsif i % 3 == 0
-            puts "Buzz #{i} is divisible 3"
-        elsif i % 5 == 0
-            puts "Fizz #{i} is divisible 5"
-        else
-            puts i
-        end
-        i += 1
-    end
-end
-
-fizzbuzz_w(36)
+fives.each_with_index do |number, index|
+    puts "The current value is #{number} at index #{index}!"
+    puts number * index
+end 
--- index.rb
+++ index.rb
@@ -1,27 +1,15 @@
-# for loop
-# for loop is not that popular in ruby as in other languages
-#  each is prefered in ruby and more secured
+colors = ["Red", "Blue", " Green", "Yellow"]
 
-
-
-for i in 0..5
-     puts "Value of local variable is #{i}"
-     
-end  
-
-puts
-puts "Vaule of local Variable #{i} can also be accessed outside unlike the each method"
-
-# so we can manipulate i and overwritten
-puts
-puts
-
-
-# for loop with next statement
-numbers = [3, 5, 7]
-
-for num in numbers
-     puts "#{num}"
+colors.each_with_index do |color, index|
+     puts "Moving on to index number #{index}"
+     puts " The Current color is #{color}"
 end
 
-p "#{num} accessed  outside again"
+# Moving on to index number 0
+#  The Current color is Red
+# Moving on to index number 1
+#  The Current color is Blue
+# Moving on to index number 2
+#  The Current color is  Green
+# Moving on to index number 3
+#  The Current color is Yellow
