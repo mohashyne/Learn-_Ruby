bfa92512f9ef02e26dbac0a09d962d096bb1d712
MSA <crystalbluetech@gmail.com>
2023-09-24 23:37:53 +0100

    each within each (nested each)

--- index.rb
+++ index.rb
@@ -1,48 +1,23 @@
-fives =[5, 10, 15, 20, 25, 30, 35, 40]
-even = []
-odd = []
-
-fives.each do |number|
-if number.even?
-     even << number
-else
-     odd << number
-end
-end 
-
-puts "we are even #{even}" # we are even [10, 20, 30, 40]
-
-puts "we are  odd #{odd}" # we are  odd [5, 15, 25, 35]
+shirts = ["striped", "plain white", "plaid", "band"]
+ties = ["polka dot", "solid color", "boring"]
 
 
-# shorten the code
-twos =[5, 10, 15, 20, 25, 30, 35, 40]
-even_2 = []
-odd_2 = []
-
-twos.each do |num|
-     num.even? ? even_2 << num : odd_2 << num
-end
-
-puts even_2
-puts odd_2
-
-# shortest code
-# twos.each {|num| num.even? ? even_2 << num : odd_2 << num }
-
-
-
-#  Using  a  method name
-printers =[2, 8, 15, 20, 25, 30, 44, 5,35]
-def print_even_and_odd(array)
-even_printer = []
-odd_printer = []
-
-array.each { |num1| num1.even? ? even_printer << num1 : odd_printer << num1 }
-
-puts "printing... even #{even_printer}"
-puts "printing... odd #{odd_printer}"
+shirts.each do |shirt|
+ ties.each do |tie|
+    puts "OPTIONS: A #{shirt} shirt and a #{tie} tie."
+ end
 
 end
 
-print_even_and_odd(printers)
+# OPTIONS: A striped shirt and a polka dot tie
+# OPTIONS: A striped shirt and a solid color tie
+# OPTIONS: A striped shirt and a boring tie
+# OPTIONS: A plain white shirt and a polka dot tie
+# OPTIONS: A plain white shirt and a solid color tie
+# OPTIONS: A plain white shirt and a boring tie
+# OPTIONS: A plaid shirt and a polka dot tie
+# OPTIONS: A plaid shirt and a solid color tie
+# OPTIONS: A plaid shirt and a boring tie
+# OPTIONS: A band shirt and a polka dot tie
+# OPTIONS: A band shirt and a solid color tie
+# OPTIONS: A band shirt and a boring tie
