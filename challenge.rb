86505f6768c93c8efeacf92ae685f4df1e36625a
MSA <crystalbluetech@gmail.com>
2023-09-25 00:50:47 +0100

    challenge and solutions each_with_index

--- challenge.rb
+++ challenge.rb
@@ -1,9 +1,40 @@
-# write a loop that iterates over a number array
-# Output the PRODUCT of each number and its index position
+# [1, 2, 3, 4, 5]
 
-fives = [5, 10, 15, 20, 25]
+# Write a loop that gives me sum of
+# the products of each index and its value
 
-fives.each_with_index do |number, index|
-    puts "The current value is #{number} at index #{index}!"
-    puts number * index
-end 
+# solution
+numbers = [1, 2, 3, 4, 5]
+
+sum = 0
+
+numbers.each_with_index do |number, index|
+    puts "The next product is #{number} with index #{index}"
+    product = number * index
+    puts product
+    sum += product
+end
+
+puts "The sum of the products is #{sum}"
+
+
+
+# challenge 2
+# [-1, 2, 1, 2, 5, 7]
+# Print  the product of the elements and its index position
+# print if the index position is greater than the element
+# create this within a method
+arr = [-1, 2, 1, 2, 5, 7]
+
+def print_if(array)
+
+    array.each_with_index do |number, idx|
+        product = number * idx
+        if idx > number
+            puts "The product of #{number} and #{idx} is #{product}"
+            puts "The index is greater than the number"
+        end
+    end
+end
+
+print_if(arr)
