cdc05967be6246851b7298ba56ce06c1847725cd
MSA <crystalbluetech@gmail.com>
2023-09-24 01:36:06 +0100

    each examples and append

--- index.rb
+++ index.rb
@@ -30,3 +30,19 @@ puts odd_2
 # shortest code
 # twos.each {|num| num.even? ? even_2 << num : odd_2 << num }
 
+
+
+#  Using  a  method name
+printers =[2, 8, 15, 20, 25, 30, 44, 5,35]
+def print_even_and_odd(array)
+even_printer = []
+odd_printer = []
+
+array.each { |num1| num1.even? ? even_printer << num1 : odd_printer << num1 }
+
+puts "printing... even #{even_printer}"
+puts "printing... odd #{odd_printer}"
+
+end
+
+print_even_and_odd(printers)
