958a56da0e9f578308da5e688e77ac552d4c8b7b
MSA <crystalbluetech@gmail.com>
2023-09-24 01:04:19 +0100

    using each method

--- index.rb
+++ index.rb
@@ -1,21 +1,41 @@
-# block is a:
-     # 1. chunk of code
-     # 2. that we can pass around to different methods
-     
-     3.times {|number| puts "Hello there #{number}"}
-# Hello there 0
-# Hello there 1
-# Hello there 2
+store = ["books", "pencils", "eraser"]
 
-     
-    5.times do |num|
-     square = num * num
-     puts "Hello from inside the block is #{num} and the square is #{square}"
-     end
-# Hello from inside the block is 0 and the square is 0
-# Hello from inside the block is 1 and the square is 1
-# Hello from inside the block is 2 and the square is 4
-# Hello from inside the block is 3 and the square is 9
-# Hello from inside the block is 4 and the square is 16
-     
-     # puts square  # will return an error since its inside a block
+store.each do |item|
+     puts "We have #{10} #{item} in each shelf"
+     puts "Buy one Get one free"
+end
+
+# We have 10 books in each shelf
+# Buy one Get one free
+# We have 10 pencils in each shelf
+# Buy one Get one free
+# We have 10 eraser in each shelf
+# Buy one Get one free
+# We have 10books in each shelf
+# We have 10pencils in each shelf
+# We have 10eraser in each shelf
+
+store.each {|items| puts "We have #{10}#{items} in each shelf" }
+# We have 10books in each shelf
+# We have 10pencils in each shelf
+# We have 10eraser in each shelf
+
+names = ["Muhammad", "aminu", "salihu"]
+names.each do |name|
+ puts name.capitalize
+end
+
+# Muhammad
+# Aminu
+# Salihu
+
+
+[1, 2, 3, 4, 5].each do |num|
+     square = num *  num
+     puts "The square of  #{num} is #{square}"
+end
+
+# The square of  2 is 4
+# The square of  3 is 9
+# The square of  4 is 16
+# The square of  5 is 25
--- tempCodeRunnerFile.rb
+++ tempCodeRunnerFile.rb
@@ -1,1 +1,3 @@
-false
+We have 10books in each shelf
+# We have 10pencils in each shelf
+# We have 10eraser in each shelf
