c8e3fb64401c1e42845af497808e7721b7d17fa8
MSA <crystalbluetech@gmail.com>
2023-09-25 20:43:46 +0100

    using next keyword with if statement or unless

--- index.rb
+++ index.rb
@@ -1,41 +1,41 @@
-# break
-
-prizes = ["Pyrite", "Pyrite", "Pyrite", "Pyrite", "Gold", "Pyrite", "Pyrite"]
-
-i = 0
-
-while i < prizes.length
-     current = prizes[i]
-     if current == "Gold"
-          puts "Yay! Found Gold!"
-          break
-     else
-          puts "#{current} is not gold!"
-     end
-     i += 1
-end
-
-# Pyrite is not gold!
-# Pyrite is not gold!
-# Pyrite is not gold!
-# Pyrite is not gold!
-# Yay! Found Gold!
+# next
 
 
-puts "-----------------"
-
-numbers = [1, 2, 3, "Hello", 5, 6, 7, 8]
+numbers = [1, 2, 3, "Hello", 5, 6,"hi", 7, 8]
 
 numbers.each do |num|
      if num.is_a?(Integer)
           puts "The square of #{num} is #{num ** 2}"
      else
-          puts "That's not a valid number, I'm done with this nonsense!"
-          break
+          puts "That's not a valid number, continue to check!"
+          next
      end
 end
 
 # The square of 1 is 1
 # The square of 2 is 4
 # The square of 3 is 9
-# That's not a valid number, I'm done with this nonsense!
+# That's not a valid number, continue to check!
+# The square of 5 is 25
+# The square of 6 is 36
+# The square of 7 is 49
+# The square of 8 is 64
+
+# example 2
+
+prizes = [50, 77, 3, 9, "hello", 8, 9, "hi"]
+
+prizes.each do |prize|
+     unless prize.is_a?(Integer)
+          next
+     else
+          puts "The square of #{prize} is #{prize ** 2}"
+     end   
+ end
+
+# The square of 50 is 2500
+# The square of 77 is 5929
+# The square of 3 is 9
+# The square of 9 is 81
+# The square of 8 is 64
+# The square of 9 is 81
