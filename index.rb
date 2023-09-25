6bdba2e631a81afe51adc209e8fae7b529384e25
MSA <crystalbluetech@gmail.com>
2023-09-25 20:50:14 +0100

    reverse method and apply bang

--- index.rb
+++ index.rb
@@ -1,41 +1,12 @@
-# next
+#  reverse
 
+p [1, 2, 3].reverse # [3, 2, 1]
+p "abc".reverse  # "cba"
+p ["A", "B", "C"].reverse # ["C", "B", "A"]
 
-numbers = [1, 2, 3, "Hello", 5, 6,"hi", 7, 8]
+queue = [4, 8, 15, 16, 23, 42]
+p queue # [4, 8, 15, 16, 23, 42]
 
-numbers.each do |num|
-     if num.is_a?(Integer)
-          puts "The square of #{num} is #{num ** 2}"
-     else
-          puts "That's not a valid number, continue to check!"
-          next
-     end
-end
+p queue.reverse!
+p queue # [42, 23, 16, 15, 8, 4]
 
-# The square of 1 is 1
-# The square of 2 is 4
-# The square of 3 is 9
-# That's not a valid number, continue to check!
-# The square of 5 is 25
-# The square of 6 is 36
-# The square of 7 is 49
-# The square of 8 is 64
-
-# example 2
-
-prizes = [50, 77, 3, 9, "hello", 8, 9, "hi"]
-
-prizes.each do |prize|
-     unless prize.is_a?(Integer)
-          next
-     else
-          puts "The square of #{prize} is #{prize ** 2}"
-     end   
- end
-
-# The square of 50 is 2500
-# The square of 77 is 5929
-# The square of 3 is 9
-# The square of 9 is 81
-# The square of 8 is 64
-# The square of 9 is 81
